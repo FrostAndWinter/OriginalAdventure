40a7154faac5811780c2cb8aa5a2bdeba673a8fb
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-09-19 18:05:36 +1200

    Created TextBox UI Component Created a textbox ui component to hold text

---
+++ src/main/java/swen/adventure/ui/components/TextBox.java
@@ -0,0 +1,88 @@
+package swen.adventure.ui.components;
+
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PGraphics;
+import swen.adventure.ui.LayoutManagers.LayoutManager;
+import swen.adventure.ui.clickable.ClickEvent;
+import swen.adventure.ui.clickable.OnClickListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by danielbraithwt on 9/19/15.
+ */
+public class TextBox extends UIComponent {
+    private static final int DEFAULT_PADDING = 20;
+
+
+    private String text;
+    private PFont font;
+    private int padding;
+
+    public TextBox(PApplet app, String text, int x, int y) {
+        super(app, x, y, 0, 0);
+
+        this.text = text;
+
+        // Create the font
+        font = applet.createFont("Arial", 16);
+
+        padding = DEFAULT_PADDING;
+    }
+
+    public void setPadding(int p) {
+        padding = p;
+    }
+
+    @Override
+    public void drawComponent(PGraphics g) {
+        int stringWidth = (int) g.textWidth(text);
+        int stringHeight = (int) (g.textAscent() + g.textDescent());
+
+        width = padding + stringWidth;
+        height = padding + stringHeight;
+
+        g.fill(0);
+        g.textFont(font);
+
+        g.text(text.toCharArray(), 0, text.length(), x + padding/2, y + stringHeight + padding/2);
+    }
+
+    @Override
+    public boolean withinBounds(int x, int y) {
+        return (x > this.x && y > this.y) && (x < this.x + this.width && y < this.y + this.height);
+    }
+
+    @Override
+    public int getWidth(PGraphics g) {
+        g.textFont(font);
+        return padding + (int) g.textWidth(text);
+    }
+
+    @Override
+    public int getHeight(PGraphics g) {
+        g.textFont(font);
+        return padding + (int) (g.textAscent() + g.textDescent());
+    }
+
+    @Override
+    protected void componentClicked(int x, int y) {
+    }
+
+    @Override
+    public void setLayoutManager(LayoutManager lm) {
+        throw new UnsupportedOperationException("Button cant use a layout manager");
+    }
+
+    @Override
+    public void addChild(UIComponent c) {
+        throw new UnsupportedOperationException("Button cant contain child ui elements");
+    }
+
+    @Override
+    public void removeChild(UIComponent c) {
+        throw new UnsupportedOperationException("Button cant contain child ui elements");
+    }
+
+}
