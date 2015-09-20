c1e70fbfc1ad0fdc169534ce346520b9f1e60ed1
Liam O'Neill <liam.mu.oneill@gmail.com>
2015-09-20 18:23:53 +1200

    BundleArray now retains the type of the value and its API has been refactored. All put methods now return itself to allow chaining calls on itself.

--- src/main/java/swen/adventure/datastorage/BundleArray.java
+++ src/main/java/swen/adventure/datastorage/BundleArray.java
@@ -2,100 +2,53 @@ package swen.adventure.datastorage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Liam O'Neill, Student ID 300312734, on 17/09/15.
  */
 public class BundleArray {
 
-    private final List<Object> storedValues = new ArrayList<>();
+    private final List<Property> storedValues = new ArrayList<>();
 
-    public void put(int value) {
-        putValue(size(), value);
-    }
+    public BundleArray put(int value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, int value) { putValue(index, value); return this; }
+    public int getInt(int index) { return getValue(index, Integer.class); }
 
-    public void put(int index, int value) {
-        putValue(index, value);
-    }
+    public BundleArray put(float value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, float value) { putValue(index, value); return this; }
+    public float getFloat(int index) { return getValue(index, Float.class); }
 
-    public int getInt(int index) {
-        return getValue(index, Integer.class);
-    }
+    public BundleArray put(String value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, String value) { putValue(index, value); return this; }
+    public String getString(int index) { return getValue(index, String.class); }
 
-    public void put(float value) {
-        putValue(size(), value);
-    }
+    public BundleArray put(long value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, long value) { putValue(index, value); return this; }
+    public long getLong(int index) { return getValue(index, Long.class); }
 
-    public void put(int index, float value) {
-        putValue(index, value);
-    }
+    public BundleArray put(BundleArray value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, BundleArray value) { putValue(index, value); return this; }
+    public BundleArray getBundleArray(int index) { return getValue(index, BundleArray.class); }
 
-    public float getFloat(int index) {
-        return getValue(index, Float.class);
-    }
-
-    public void put(String value) {
-        putValue(size(), value);
-    }
-
-    public void put(int index, String value) {
-        putValue(index, value);
-    }
-
-    public String getString(int index) {
-        return getValue(index, String.class);
-    }
-
-    public void put(long value) {
-        putValue(size(), value);
-    }
-
-    public void put(int index, long value) {
-        putValue(index, value);
-    }
-
-    public long getLong(int index) {
-        return getValue(index, Long.class);
-    }
-
-    public void put(BundleArray value) {
-        putValue(size(), value);
-    }
-
-    public void put(int index, BundleArray value) {
-        putValue(index, value);
-    }
-
-    public BundleArray getBundleArray(int index) {
-        return getValue(index, BundleArray.class);
-    }
-
-    public void put(BundleObject value) {
-        putValue(size(), value);
-    }
-
-    public void put(int index, BundleObject value) {
-        putValue(index, value);
-    }
-
-    public BundleObject getBundleObject(int index) {
-        return getValue(index, BundleObject.class);
-    }
+    public BundleArray put(BundleObject value) { putValue(size(), value); return this; }
+    public BundleArray put(int index, BundleObject value) { putValue(index, value); return this; }
+    public BundleObject getBundleObject(int index) { return getValue(index, BundleObject.class); }
 
     private <T> T getValue(int index, Class<T> class0) {
         if(index < 0 || index >= size())
             throw new IllegalArgumentException("index is out of bounds " + index);
 
-        Object value = storedValues.get(index);
+        Property property = storedValues.get(index);
 
-        if(!class0.isInstance(value))
+        if(class0 != property.class0)
             throw new ClassCastException("Property at index" + index + " isn't a type of " + class0.getSimpleName());
 
-        return class0.cast(value);
+        return class0.cast(property.value);
     }
 
     private void putValue(int index, Object value) {
-        storedValues.add(index, value);
+        storedValues.add(index, new Property(Integer.toString(index), value, value.getClass()));
     }
 
     public int size() {
@@ -120,6 +73,9 @@ public class BundleArray {
 
     @Override
     public String toString() {
-        return "BundleObject={" + storedValues + "}";
+        List<String> toStrings = storedValues.stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+        return "BundleArray{" + String.join(",", toStrings) + '}';
     }
 }
