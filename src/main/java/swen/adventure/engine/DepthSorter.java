FrostAndWinter/OriginalAdventure

commit 70bc57e1d68547c52e27ad394d7741e91826aefe

Displays All Tooltips Now displays all the tooltips nicely centered

src/main/java/swen/adventure/game/AdventureGame.java
@@ -208,21 +208,16 @@ public class AdventureGame implements Game {
             _glRenderer.render(meshNodesSortedByZ, _sceneGraph.allNodesOfType(Light.class), cameraNode.worldToNodeSpaceTransform(), cameraNode.fieldOfView(), cameraNode.hdrMaxIntensity());
         });
 
-        String tooltip = "";
-        for (Interaction interaction : _possibleInteractionsForStep.values()) {
-            Interaction.ActionType actionType = interaction.interactionType.actionType;
-            Character character = null;
-            switch (actionType) {
-                case Primary:
-                    character = _keyInput.characterForEvent(_keyInput.eventPrimaryAction);
-                    break;
-                case Secondary:
-                    character = _keyInput.characterForEvent(_keyInput.eventSecondaryAction);
-                    break;
+        ArrayList<String> tips = new ArrayList<>();
+        for (Interaction.InteractionType t : _possibleInteractionsForStep.keySet()) {
+            if (_possibleInteractionsForStep.containsKey(t)) {
+                Interaction i = _possibleInteractionsForStep.get(t);
+
+                tips.add(String.format("Press %s to %s %s", "{{key}}", i.interactionType.toString(), i.gameObject.id));
             }
-            tooltip += interaction.interactionMessageForObjectAndButton(_player, character) + "\n";
         }
-        ui.setTooltip(tooltip);
+
+        ui.setTooltip(tips);
 
         ui.drawUI(_pGraphics, _glRenderer);
     }

src/main/java/swen/adventure/game/ui/components/UI.java
@@ -11,25 +11,30 @@ import swen.adventure.engine.ui.layoutmanagers.LinearLayout;
 import swen.adventure.game.scenenodes.Inventory;
 import swen.adventure.game.scenenodes.Player;
 
+import java.util.ArrayList;
+
 /**
  * Created by danielbraithwt on 10/10/15.
  */
 public class UI extends Frame {
-    Panel _container;
-    InventoryComponent _inventory;
-    ControlsOverlay _controlsOverlay;
-    TextBox _tooltip;
+    private Panel _container;
+    private InventoryComponent _inventory;
+    private ControlsOverlay _controlsOverlay;
+    private ArrayList<String> _tooltips;
+    private Reticule _reticule;
 
     public UI(int w, int h, Player p) {
         super(0, 0, w, h);
+
+        _tooltips = new ArrayList<>();
 
         _container = new Panel(x,y,w,h);
         _container.setColor(new Color(0,0,0,0));
 
         // Create the reticule so the user can see where they are looking
         int size = 5;
-        Reticule reticule = new Reticule(width/2, height/2, size);
-        _container.addChild(reticule);
+        _reticule = new Reticule(width/2, height/2, size);
+        _container.addChild(_reticule);
 
         // Set up the inventory
         _inventory = new InventoryComponent(p.inventory(), 275, 500);
@@ -40,20 +45,21 @@ public class UI extends Frame {
         _controlsOverlay.setVisible(false);
         _container.addChild(_controlsOverlay);
 
-        _tooltip = new TextBox("TOOLTIP!!", 400, 400);
-        _tooltip.setVisible(false);
-        _container.addChild(_tooltip);
-
         addChild(_container);
     }
 
-    public void setTooltip(String tip) {
-        _tooltip.setText(tip);
-        _tooltip.setVisible(true);
+    public void setTooltip(ArrayList<String> tips) {
+        removeTooltip();
+
+        if (tips == null) {
+            return;
+        }
+
+        _tooltips.addAll(tips);
     }
 
     public void removeTooltip() {
-        _tooltip.setVisible(false);
+        _tooltips.clear();
     }
 
     public void setControlsOverlayVisibility(boolean b) {
@@ -76,7 +82,21 @@ public class UI extends Frame {
         pg.noStroke();
         pg.translate(dw, dh);
 
+        // Draw the components
         draw(pg, scale, scale);
+
+        // Draw the tool tips
+        pg.fill(255);
+        float tooltipX = pg.width/2f;
+        float tooltipY = pg.height/2f + _reticule.getWidth(pg)* 2;
+
+        float h = pg.textAscent() + pg.textDescent();
+        for (String s : _tooltips) {
+            float w = pg.textWidth(s);
+
+            pg.text(s, tooltipX - w/2, tooltipY);
+            tooltipY += h;
+        }
 
         pg.endDraw();
 

commit 1b037d808ec2678068be0d44d256cac93b6962a4

Fixed the interactions not using the correct key (fixed a regression introducted in 70bc57e1d68547c52e27ad394d7741e91826aefe)

src/main/java/swen/adventure/engine/DepthSorter.java
@@ -33,7 +33,7 @@ public class DepthSorter {
          * @return -1 if this object's maximum z is closer to the camera than other's, 0 if they are equal, or 1 if it is further from the camera.
          */
         public int compareTo(SortableMesh other) {
-            return (int)(other.boundingBox.maxPoint.z - this.boundingBox.maxPoint.z);
+            return Float.compare(other.boundingBox.maxPoint.z, this.boundingBox.maxPoint.z);
         }
     }
 

src/main/java/swen/adventure/engine/datastorage/SceneGraphSerializer.java
@@ -1,6 +1,5 @@
 package swen.adventure.engine.datastorage;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -81,7 +80,7 @@ public class SceneGraphSerializer {
         else
             throw new RuntimeException("Don't recognise node " + sceneNode);
 
-        sceneNode.getChildren()
+        sceneNode.children()
                 .forEach(node -> serializeSceneNode(node, serializedNode));
     }
 

src/main/java/swen/adventure/engine/scenegraph/SceneNode.java
@@ -1,5 +1,7 @@
 package swen.adventure.engine.scenegraph;
 
+import javafx.scene.Scene;
+import swen.adventure.engine.Action;
 import swen.adventure.engine.Event;
 import swen.adventure.engine.datastorage.BundleObject;
 import swen.adventure.engine.datastorage.BundleSerializable;
@@ -35,6 +37,14 @@ public abstract class SceneNode implements BundleSerializable {
     public final String id;
     protected Map<String, SceneNode> _idsToNodesMap;
     protected Map<Class<? extends SceneNode>, List<? extends SceneNode>> _nodesOfTypeMap;
+
+    public static Action<SceneNode, SceneNode, SceneNode> actionSetEnabled = (ignored, ignored1, sceneNode, data) -> {
+        sceneNode.setEnabled(true);
+    };
+
+    public Action<SceneNode, SceneNode, SceneNode> actionSetDisabled = (ignored, ignored1, sceneNode, data) -> {
+        sceneNode.setEnabled(false);
+    };
 
     @Override
     public BundleObject toBundle() {
@@ -223,7 +233,7 @@ public abstract class SceneNode implements BundleSerializable {
         _parent = Optional.ofNullable(newParent);
     }
 
-    public Set<SceneNode> getChildren() {
+    public Set<SceneNode> children() {
         return Collections.unmodifiableSet(_childNodes);
     }
 
@@ -231,7 +241,9 @@ public abstract class SceneNode implements BundleSerializable {
         return _isEnabled;
     }
 
+    /** Recursively sets isEnabled on this node's children and itself. */
     public void setEnabled(boolean isEnabled) {
+        this.children().forEach(node -> node.setEnabled(isEnabled));
         _isEnabled = isEnabled;
     }
 

src/main/java/swen/adventure/game/AdventureGame.java
@@ -13,9 +13,6 @@ import swen.adventure.engine.rendering.GLRenderer;
 import swen.adventure.engine.rendering.PickerRenderer;
 import swen.adventure.engine.rendering.maths.Quaternion;
 import swen.adventure.engine.scenegraph.*;
-import swen.adventure.engine.ui.color.Color;
-import swen.adventure.engine.ui.components.*;
-import swen.adventure.engine.ui.layoutmanagers.LinearLayout;
 import swen.adventure.game.input.AdventureGameKeyInput;
 import swen.adventure.game.input.AdventureGameMouseInput;
 import swen.adventure.game.scenenodes.*;
@@ -209,12 +206,17 @@ public class AdventureGame implements Game {
         });
 
         ArrayList<String> tips = new ArrayList<>();
-        for (Interaction.InteractionType t : _possibleInteractionsForStep.keySet()) {
-            if (_possibleInteractionsForStep.containsKey(t)) {
-                Interaction i = _possibleInteractionsForStep.get(t);
-
-                tips.add(i.interactionMessageForObjectAndButton(_player, 'q'));
+        for (Interaction interaction : _possibleInteractionsForStep.values()) {
+            Interaction.ActionType actionType = interaction.interactionType.actionType;
+            Character character = null;
+            switch (actionType) {
+                case Primary:
+                    character = _keyInput.characterForEvent(_keyInput.eventPrimaryAction);break;
+                case Secondary:
+                    character = _keyInput.characterForEvent(_keyInput.eventSecondaryAction);
+                    break;
             }
+            tips.add(interaction.interactionMessage(_player, character));
         }
 
         ui.setTooltip(tips);

src/main/java/swen/adventure/game/Interaction.java
@@ -63,7 +63,7 @@ public final class Interaction {
         this.gameObject.eventInteractionEnded.trigger(player, Collections.singletonMap(EventDataKeys.Interaction, this));
     }
 
-    public String interactionMessageForObjectAndButton(Player player, Character buttonName) {
+    public String interactionMessage(Player player, Character buttonName) {
         switch (this.interactionType) {
             case PickUp:
                 return String.format("Press %c to pick up %s", buttonName, this.gameObject.name);

src/main/java/swen/adventure/game/scenenodes/Key.java
@@ -14,14 +14,6 @@ public class Key extends Item {
 
     private static final String KeyDescription = "A key. Perhaps it opens a door?";
 
-    public Action<Puzzle, Puzzle, GameObject> actionSetEnabled = (puzzle, ignored, key, data) -> {
-        this.setEnabled(true);
-    };
-
-    public Action<Puzzle, Puzzle, GameObject> actionSetDisabled = (puzzle, ignored, key, data) -> {
-        this.setEnabled(false);
-    };
-
     public Key(String id, TransformNode parent) {
         super(id, parent, "Key", KeyDescription);
 

src/main/resources/EventConnections.event
@@ -3,8 +3,8 @@ LeverToggled; greenLightLever; ToggleLight; greenTorchFlame
 LeverToggled; redLightLever; ToggleLight; redTorchFlame
 LeverToggled; blueLightLever; ToggleLight; blueTorchFlame
 LightToggled; blueTorchFlame, redTorchFlame, greenTorchFlame; CheckPuzzle; keyPuzzle
-PuzzleSolved; keyPuzzle; SetEnabled; key
-PuzzleUnsolved; keyPuzzle; SetDisabled; key
+PuzzleSolved; keyPuzzle; SetEnabled; keyLocationT
+PuzzleUnsolved; keyPuzzle; SetDisabled; keyLocationT
 
 //Make the lever go back up when interaction ends.
 InteractionEnded; greenLightLever; MoveUp; greenLightLever

src/main/resources/SceneGraph.xml
@@ -1,9 +1,9 @@
 <root>
-    <TransformNode id="groundPlaneTransform" rotation="-0.70710677, 0.0, 0.0, 0.70710677" scale="1200, 1800, 0.2">
+    <TransformNode id="groundPlaneTransform" rotation="-0.70710677, 0.0, 0.0, 0.70710677" scale="600, 900, 0.2">
         <MeshNode id="groundPlaneMesh" fileName="Plane.obj" materialDirectory="MedievalModels" materialFileName="MedievalMaterials.mtl" materialName="corner_fullSG" textureRepeat="24, 36, 0.2"/>
     </TransformNode>
 
-    <TransformNode id="ceilingPlaneTransform" translation="0, 330, 0" rotation="0.70710677, 0.0, 0.0, 0.70710677" scale="1200, 1800, 0.2">
+    <TransformNode id="ceilingPlaneTransform" translation="0, 280, 0" rotation="0.70710677, 0.0, 0.0, 0.70710677" scale="600, 900, 0.2">
         <MeshNode id="ceilingPlaneMesh" fileName="Plane.obj" materialFileName="StoneWall.mtl" materialName="StoneWall" textureRepeat="10, 10, 0.2"/>
     </TransformNode>
     
@@ -14,6 +14,8 @@
         <Player id="player" boundingBox="-30, -60, -10, 30, 60, 10"/>
     </TransformNode>
     <AmbientLight id="ambientLight" colour="0.5, 0.5, 0.5" intensity="1.0"/>
+
+    <!-- Room 1 starts here. Centred at origin, dimensions are 1200 x 1800. -->
     
     <TransformNode id="greenLightLeverTransform" translation="-600, 110, 90" rotation="0, 0, -0.7071067812, 0.7071067812" scale="0.5, 0.5, 0.5">
         <Lever id="greenLightLever"/>
@@ -41,27 +43,27 @@
     </TransformNode>
 
     <TransformNode id="wallsTransform" scale="1, 1.4, 1">
-        <TransformNode id="leftWallTransform" translation="-600, 0, 0" rotation="0, 0.7071067812, 0, 0.7071067812" scale="20, 1, 1">
-            <MeshNode id="leftWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="20, 1, 1" isCollidable="true"/>
+        <TransformNode id="leftWallTransform" translation="-600, 0, 0" rotation="0, 0.7071067812, 0, 0.7071067812" scale="18, 1, 1">
+            <MeshNode id="leftWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="18, 1, 1" isCollidable="true"/>
         </TransformNode>
-        <TransformNode id="rightWallTransform" translation="600, 0, 0" rotation="0, -0.7071067812, 0, 0.7071067812" scale="20, 1, 1">
-            <MeshNode id="rightWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="20, 1, 1" isCollidable="true"/>
+        <TransformNode id="rightWallTransform" translation="600, 0, 0" rotation="0, -0.7071067812, 0, 0.7071067812" scale="18, 1, 1">
+            <MeshNode id="rightWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="18, 1, 1" isCollidable="true"/>
         </TransformNode>
-        <TransformNode id="backWallTransform" translation="0, 0, 900" rotation="0, 1, 0, 0" scale="13.33333, 1, 1">
-            <MeshNode id="backWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="13.33333, 1, 1" isCollidable="true"/>
+        <TransformNode id="backWallTransform" translation="0, 0, 900" rotation="0, 1, 0, 0" scale="12, 1, 1">
+            <MeshNode id="backWall" directory="MedievalModels" fileName="Wall.obj" textureRepeat="12, 1, 1" isCollidable="true"/>
         </TransformNode>
 
 
         <TransformNode id="frontWallTransform" translation="0, 0, -900">
-            <TransformNode id="doorTransform" scale="1.4, 1, 1.7" translation="230, 0.3, 0">
+            <TransformNode id="doorTransform" scale="1.4, 1, 1.7" translation="230, 0.3, 0"> <!-- width is 140 units. -->
                 <Door id="door"/>
             </TransformNode>
 
-            <TransformNode id="frontWallLeftTransform" translation="-240, 0, 0" scale="8, 1, 1">
-                <MeshNode id="frontWallLeft" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="8, 1, 1" isCollidable="true"/>
+            <TransformNode id="frontWallLeftTransform" translation="-490, 0, 0" scale="13, 1, 1">
+                <MeshNode id="frontWallLeft" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="13, 1, 1" isCollidable="true"/>
             </TransformNode>
-            <TransformNode id="frontWallRightTransform" translation="450, 0, 0" scale="3.2, 1, 1">
-                <MeshNode id="frontWalRight" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="3.2, 1, 1" isCollidable="true"/>
+            <TransformNode id="frontWallRightTransform" translation="700, 0, 0" scale="8.2, 1, 1">
+                <MeshNode id="frontWalRight" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="8.2, 1, 1" isCollidable="true"/>
             </TransformNode>
         </TransformNode>
     </TransformNode>
@@ -101,8 +103,7 @@
 
     <TransformNode id="wallTorchesTransform" translation="0, -20, 840" rotation="0, 1, 0, 0">
 
-
-        <TransformNode id="keyLocationT" translation="0, 165, -46">
+        <TransformNode id="keyLocationT" translation="0, 165, -46" isDynamic="true">
             <TransformNode id="keyT" scale="5, 5, 5" isDynamic="true">
                 <Key id="key" enabled="false"/>
             </TransformNode>
@@ -133,5 +134,60 @@
     </TransformNode>
 
     <Puzzle id="keyPuzzle" conditions="redTorchFlame, isOn, true; greenTorchFlame, isOn, false; blueTorchFlame, isOn, true"/>
+
+    <!-- Room 2 starts here. -->
+
+    <TransformNode id="room2" translation="0, 0, -1200">
+        <TransformNode id="room2GroundAndCeiling" scale="1100, 1.4, 300">
+            <TransformNode id="r2GroundPlaneT" rotation="-0.70710677, 0.0, 0.0, 0.70710677">
+                <MeshNode id="r2GroundPlane" fileName="Plane.obj" materialDirectory="MedievalModels" materialFileName="MedievalMaterials.mtl" materialName="corner_fullSG" textureRepeat="44, 12, 0.2"/>
+            </TransformNode>
+
+            <TransformNode id="r2CeilingPlaneT" translation="0, 200, 0" rotation="0.70710677, 0.0, 0.0, 0.70710677">
+                <MeshNode id="r2CeilingPlane" fileName="Plane.obj" materialFileName="StoneWall.mtl" materialName="StoneWall" textureRepeat="44, 12, 0.2"/>
+            </TransformNode>
+        </TransformNode>
+
+            <TransformNode id="room2LeftWall" scale="6, 1.4, 1" translation="-1100, 0, 0" rotation="0, 0.7071067812, 0, 0.7071067812">
+                <MeshNode id="room2LeftWallM" directory="MedievalModels" fileName="Wall.obj" textureRepeat="6, 1.4, 1" isCollidable="true"/>
+            </TransformNode>
+
+            <TransformNode id="room2RightWall" scale="6, 1.4, 1" translation="1100, 0, 0" rotation="0, -0.7071067812, 0, 0.7071067812">
+                <MeshNode id="room2RightWallM" directory="MedievalModels" fileName="Wall.obj" textureRepeat="6, 1.4, 1" isCollidable="true"/>
+            </TransformNode>
+
+        <TransformNode id="room2FrontWallLeft" scale="5, 1.4, 1" translation="-850, 0, -300">
+            <MeshNode id="room2FrontWallLeftM" directory="MedievalModels" fileName="Wall.obj" textureRepeat="5, 1.4, 1" isCollidable="true"/>
+        </TransformNode>
+
+        <TransformNode id="room2FrontWallRight" scale="5, 1.4, 1" translation="850, 0, -300">
+            <MeshNode id="room2FrontWallRightM" directory="MedievalModels" fileName="Wall.obj" textureRepeat="5, 1.4, 1" isCollidable="true"/>
+        </TransformNode>
+
+
+    </TransformNode>
+
+    <TransformNode id="longCorridors" translation="0, 0, -4260">
+        <TransformNode id="corridorsGroundAndCeiling" scale="600, 1.4, 2760">
+            <TransformNode id="corridorsGroundPlaneT" rotation="-0.70710677, 0.0, 0.0, 0.70710677">
+                <MeshNode id="corridorsGroundPlane" fileName="Plane.obj" materialDirectory="MedievalModels" materialFileName="MedievalMaterials.mtl" materialName="corner_fullSG" textureRepeat="12, 55.2, 0.2"/>
+            </TransformNode>
+
+            <TransformNode id="corridorsCeilingPlaneT" translation="0, 200, 0" rotation="0.70710677, 0.0, 0.0, 0.70710677">
+                <MeshNode id="corridorsCeilingPlane" fileName="Plane.obj" materialFileName="StoneWall.mtl" materialName="StoneWall" textureRepeat="12, 110.4, 0.2"/>
+            </TransformNode>
+        </TransformNode>
+
+        <TransformNode id="corridorsCentreColumns" rotation="0, -0.7071067812, 0, 0.7071067812">
+            <TransformNode id="corridorsLeftColumn" scale="55.2, 0.8, 1" translation="0, 0, 120">
+                <MeshNode id="corridorsLeftColumnBottomMesh" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="27.6, 0.8, 1" isCollidable="true"/>
+            </TransformNode>
+
+            <TransformNode id="corridorsRightColumn" scale="55.2, 0.8, 1" translation="0, 0, -120">
+                <MeshNode id="corridorsRightColumnBottomMesh" directory="MedievalModels" fileName="WallColumnMiddle.obj" textureRepeat="27.6, 0.8, 1" isCollidable="true"/>
+            </TransformNode>
+        </TransformNode>
+
+    </TransformNode>
     
 </root>