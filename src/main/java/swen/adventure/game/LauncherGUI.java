c1b183af8076488a621eafad741676d502a0ecdc
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-10-14 13:51:45 +1300

    Launcher Now Starts Game Can now start the game using the launcher

--- src/main/java/swen/adventure/game/LauncherGUI.java
+++ src/main/java/swen/adventure/game/LauncherGUI.java
@@ -1,10 +1,23 @@
 package swen.adventure.game;
 
 import oracle.jrockit.jfr.JFR;
+import swen.adventure.engine.GameDelegate;
+import swen.adventure.engine.Utilities;
+import swen.adventure.engine.network.Client;
+import swen.adventure.engine.network.DumbClient;
+import swen.adventure.engine.network.EventBox;
+import swen.adventure.engine.network.NetworkClient;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by danielbraithwt on 10/14/15.
@@ -16,6 +29,9 @@ public class LauncherGUI extends JFrame {
 
     private JLabel serverAddressLabel;
     private JTextField serverAddressTextField;
+
+    private JLabel serverPortLabel;
+    private JTextField serverPortTextField;
 
     private JButton start;
 
@@ -43,10 +59,29 @@ public class LauncherGUI extends JFrame {
         serverAddressTextField.setPreferredSize(new Dimension(150, 20));
         serverAddressPanel.add(serverAddressTextField, BorderLayout.CENTER);
 
+        JPanel serverPortPanel = new JPanel();
+        serverPortPanel.setLayout(new BorderLayout());
+        serverPortLabel = new JLabel("Server Port");
+        serverPortPanel.add(serverPortLabel, BorderLayout.NORTH);
+
+        serverPortTextField = new JTextField();
+        serverPortTextField.setPreferredSize(new Dimension(150, 20));
+        serverPortPanel.add(serverPortTextField, BorderLayout.CENTER);
+
         start = new JButton("Start Game");
+        start.addActionListener(e -> {
+            startGame(true);
+        });
+
+        JButton startSingle = new JButton("Start Single Player");
+        startSingle.addActionListener(e -> {
+            startGame(false);
+        });
 
         input.add(serverAddressPanel);
+        input.add(serverPortPanel);
         input.add(start);
+        input.add(startSingle);
 
         setLayout(new BorderLayout());
 
@@ -57,7 +92,21 @@ public class LauncherGUI extends JFrame {
         setVisible(true);
     }
 
+    public void startGame(boolean n) {
+        String[] args;
+        if (n) {
+            args = new String[] {nameTextField.getText(), serverAddressTextField.getText(),
+                                serverPortTextField.getText()};
+        } else {
+            args = new String[] {};
+        }
+
+        setVisible(false);
+
+        AdventureGame.startGame(args);
+    }
+
     public static void main(String[] args) {
-        new LauncherGUI();
+        LauncherGUI l = new LauncherGUI();
     }
 }
